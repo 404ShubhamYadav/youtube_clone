1e4e24fb2cf39a78965cf8ccb3ba541bc2ef13a6
Shubham Kr. Yadav <shubhamyadav5594@gmail.com>
2024-06-19 12:48:17 +0530

    fixed bugs

--- src/componets/Head.js
+++ src/componets/Head.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { toggleMenu } from "../utils/appSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { YOUTUBE_SEARCH_API } from "../utils/constant";
-import {cacheResults} from "../utils/searchSlice"
+import { cacheResults } from "../utils/searchSlice"
 
 const Head = () => {
     const [searchQuery, setSearchQuery] = useState(" ");
@@ -10,27 +10,27 @@ const Head = () => {
     const [showSuggestions, setShowSuggestions] = useState(false);
     // const [onHover, setOnhover] = useState(false);
 
-    const searchCache = useSelector(store=>store.search);
+    const searchCache = useSelector(store => store.search);
     const dispatch = useDispatch();
 
     // API Call
-        // Make API call after every key press
-        // but if the difference between 2 APIs call<200ms decline the api call
-        // This is debouncing
+    // Make API call after every key press
+    // but if the difference between 2 APIs call<200ms decline the api call
+    // This is debouncing
     /**
      * searchCatche = {
      * "iphone": ["iphone11, "iphone14"]
      * }
      * searchQuery = Iphone
      * */
-    
+
     useEffect(() => {
         const timer = setTimeout(() => {
-        if(searchCache[searchQuery]){
-            setSuggestions(searchCache[searchQuery]);
-        } else{
-            getSearchSuggestions();
-        }
+            if (searchCache[searchQuery]) {
+                setSuggestions(searchCache[searchQuery]);
+            } else {
+                getSearchSuggestions();
+            }
         }, 200);
 
         return () => {
@@ -59,8 +59,14 @@ const Head = () => {
     return (
         <div className="grid grid-flow-col p-3 m-2 shadow-lg">
             <div className="flex col-span-2 ">
-                <img onClick={() => toggleMenuHandler()} className=" h-8 w-10 cursor-pointer " src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/800px-Hamburger_icon.svg.png" alt="menu" />
-                <img className="h-14 w-15 mx-1" src="https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6-1200-80.jpg" alt="logo" />
+                <img
+                    onClick={() => toggleMenuHandler()}
+                    className=" h-12 w-10 cursor-pointer "
+                    src="https://cdn3.iconfinder.com/data/icons/iconae-light/100/grid-hamburgermenu-menu-menubutton-toggle-togglemenu-ui-512.png" alt="menu" />
+                    
+                <img
+                    className="h-12 w-15 mx-2"
+                    src="https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6-1200-80.jpg" alt="logo" />
             </div>
             <div className="col-span-10 px-9">
                 <div>
@@ -69,19 +75,19 @@ const Head = () => {
                         type="text"
                         value={searchQuery}
                         onChange={(e) => setSearchQuery(e.target.value)}
-                        onFocus={() => setShowSuggestions(true)} 
-                        onBlur={()=> setShowSuggestions(false)}                       
-                        />                  
+                        onFocus={() => setShowSuggestions(true)}
+                        onBlur={() => setShowSuggestions(false)}
+                    />
                     <button className="border border-gray-400 px-5 py-2 rounded-r-full">
                         🔍
                     </button>
                 </div>
-                {showSuggestions &&  (
-                <div className="fixed bg-white py-2 px-2 w-[31rem] shadow-lg rounded-lg border border-gray-100">
-                    <ul>
-                        {suggestions.map(s=><li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100"> 🔍{s}</li>)}
-                    </ul>
-                </div>
+                {showSuggestions && (
+                    <div className="fixed bg-white py-2 px-2 w-[31rem] shadow-lg rounded-lg border border-gray-100">
+                        <ul>
+                            {suggestions.map(s => <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100"> 🔍{s}</li>)}
+                        </ul>
+                    </div>
                 )}
             </div>
             <div>
